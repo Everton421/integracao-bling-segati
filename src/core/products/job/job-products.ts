65b6d00479d8588e5d07da10f8ba2359106f1d35
unknown <evertonjose421@gmail.com>
2026-05-29 09:58:27 -0300

    29052026

--- src/core/products/job/job-products.ts
+++ src/core/products/job/job-products.ts
@@ -6,17 +6,26 @@ import { SyncProduct } from "../services/sync-product";
 export class JobProducts{
  
     static   async jobPostOrUpdateAllProducts(){
-        const allProducts = await ProdutoRepository.buscaProdutos();
-                let dadosConfig = await ApiConfigRepository.buscaConfig();
+        const allProducts = await ProdutoRepository.buscaProduto(16102);
+
+        let dadosConfig = await ApiConfigRepository.buscaConfig();
+        
         if (dadosConfig[0].enviar_produtos === 'E') {
 
         const syncproductService = new SyncProduct();
 
+            let processados = 1;
+
             if(allProducts.length > 0 ){
+                console.log(`[V] ${allProducts.length} produtos encontrados...`)
+
                 for(const product of allProducts){
                     const resultService = await syncproductService.postOrPutProductBling( product.CODIGO, false) 
                         console.log(resultService)
-                }
+                        console.log(`  itens processados  ${processados} de ${allProducts.length}...  `)
+                        processados++;
+                    }
+                    console.log(`[V] Fim da terefa, Total processados (${processados})`)
             }
         }else{
             return console.log("[X] A integração nao esta habilitada para envio de produtos.")
