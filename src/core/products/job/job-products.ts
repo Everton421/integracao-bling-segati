47860ef757a26bd6263df21babf97521db17230b
unknown <evertonjose421@gmail.com>
2026-05-29 09:53:58 -0300

    29052026

--- src/core/products/job/job-products.ts
+++ src/core/products/job/job-products.ts
@@ -1,4 +1,5 @@
 import { JobProduto } from "../../../jobs/job-produto";
+import { ApiConfigRepository } from "../../company/data/api-config-repository";
 import { ProdutoRepository } from "../data/produto-repository";
 import { SyncProduct } from "../services/sync-product";
 
@@ -6,20 +7,29 @@ export class JobProducts{
  
     static   async jobPostOrUpdateAllProducts(){
         const allProducts = await ProdutoRepository.buscaProdutos();
-        
+                let dadosConfig = await ApiConfigRepository.buscaConfig();
+        if (dadosConfig[0].enviar_produtos === 'E') {
+
         const syncproductService = new SyncProduct();
 
-        if(allProducts.length > 0 ){
-            for(const product of allProducts){
-                const resultService = await syncproductService.postOrPutProductBling( product.CODIGO, false) 
-                    console.log(resultService)
+            if(allProducts.length > 0 ){
+                for(const product of allProducts){
+                    const resultService = await syncproductService.postOrPutProductBling( product.CODIGO, false) 
+                        console.log(resultService)
+                }
             }
-        }
+        }else{
+            return console.log("[X] A integração nao esta habilitada para envio de produtos.")
+        } 
+
     }
 
     static async   jobGetVinculoProduct(){
-     const jobProduto = new JobProduto();
-        await jobProduto.jobSyncAllProductsFromBling();
-    }
+            
+        let dadosConfig = await ApiConfigRepository.buscaConfig();
+
+            const jobProduto = new JobProduto();
+                await jobProduto.jobSyncAllProductsFromBling();
+            }
 
 }
