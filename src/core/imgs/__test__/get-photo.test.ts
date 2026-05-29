21669d447fa3b37ea46f12d00c9bdb9d7581fbd8
unknown <evertonjose421@gmail.com>
2026-05-29 09:59:28 -0300

    29052026

--- src/core/imgs/__test__/get-photo.test.ts
+++ src/core/imgs/__test__/get-photo.test.ts
@@ -8,10 +8,10 @@ import { UploadAndInsertPhotoService } from '../services/upload-photo-service';
 test.it("Deve listar as fotos do diretório", async () => {
 
 
-    const diretorioFotos = "C:/Users/usuario/Desktop/apps/integracao-bling-segati/src/assets/imgs";
-    const referenciaProduto = "23B857521A";
-   
-   const resultPhotos =  await UploadAndInsertPhotoService.exec(diretorioFotos, referenciaProduto);
-    console.log(resultPhotos);
+   // const diretorioFotos = "C:/Users/usuario/Desktop/apps/integracao-bling-segati/src/assets/imgs";
+   // const referenciaProduto = "23B857521A";
+   //
+   //const resultPhotos =  await UploadAndInsertPhotoService.exec(diretorioFotos, referenciaProduto);
+   // console.log(resultPhotos);
 
 });
